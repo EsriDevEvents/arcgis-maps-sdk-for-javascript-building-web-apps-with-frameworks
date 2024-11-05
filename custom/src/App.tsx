9bb9570b3d11cc9974376fc76e511b0d4d8a3f99
Calin Arens <calina@geoinfo.dk>
2024-11-05 12:50:45 +0100

    Version 1

---
+++ custom/src/App.tsx
@@ -0,0 +1,77 @@
+import { defineCustomElements as defineMapElements } from "@arcgis/map-components/dist/loader";
+import {
+  ArcgisHome,
+  ArcgisMap,
+  ArcgisZoom,
+  ArcgisCoordinateConversion,
+} from "@arcgis/map-components-react";
+import {
+  CalciteNavigation,
+  CalciteNavigationLogo,
+  CalciteShell,
+} from "@esri/calcite-components-react";
+import { defineCustomElements as defineCalciteElements } from "@esri/calcite-components/dist/loader";
+import type MapView from "@arcgis/core/views/MapView";
+import { useState } from "react";
+import "./App.css";
+import SimpleCoordinateConversion from "./components/SimpleCoordinateConversion";
+import useCoordinateConversionViewModel from "./components/useCoordinateConversionViewModel";
+import useTime from "./components/useTime";
+
+// Load all map assets
+defineMapElements(window, {
+  resourcesUrl: "https://js.arcgis.com/map-components/4.30/assets",
+});
+
+// Load all Calcite assets
+defineCalciteElements(window, {
+  resourcesUrl: "https://js.arcgis.com/calcite-components/2.13.2/assets",
+});
+
+function App() {
+  // State management
+  const [mapView, setMapView] = useState<MapView>();
+
+  const handleViewReady = (event: CustomEvent<void>) => {
+    // Change state
+    const mapEl = event.target as HTMLArcgisMapElement;
+    setMapView(mapEl.view);
+  };
+
+  const { coordinateConversionViewModel } =
+    useCoordinateConversionViewModel(mapView);
+
+  const time = useTime();
+
+  // COMPOSITION OF COMPONENTS
+  return (
+    <CalciteShell>
+      <CalciteNavigation navigation-action slot="header" id="nav">
+        <CalciteNavigationLogo
+          icon="clustering"
+          slot="logo"
+          heading="Custom hooks and using Calcite components"
+          description="Esri European Developer Summit 2024"
+        ></CalciteNavigationLogo>
+        <p slot="content-end">{time.toLocaleTimeString()}</p>
+      </CalciteNavigation>
+      <ArcgisMap
+        itemId="05e015c5f0314db9a487a9b46cb37eca"
+        popupDisabled
+        onArcgisViewReadyChange={handleViewReady}
+      >
+        {/* INCLUDE HOME, ZOOM AND COORDINATECONVERSION COMPONENTS */}
+        <ArcgisHome position="top-left"></ArcgisHome>
+        <ArcgisZoom position="top-left"></ArcgisZoom>
+        <ArcgisCoordinateConversion position="bottom-left"></ArcgisCoordinateConversion>
+        {coordinateConversionViewModel && (
+          <SimpleCoordinateConversion
+            vm={coordinateConversionViewModel}
+          ></SimpleCoordinateConversion>
+        )}
+      </ArcgisMap>
+    </CalciteShell>
+  );
+}
+
+export default App;
---
+++ custom/src/components/SimpleCoordinateConversion.tsx
@@ -0,0 +1,119 @@
+import CoordinateConversionViewModel from "@arcgis/core/widgets/CoordinateConversion/CoordinateConversionViewModel";
+import Conversion from "@arcgis/core/widgets/CoordinateConversion/support/Conversion.js";
+import {
+  CalciteButton,
+  CalciteInlineEditable,
+  CalciteInput,
+  CalciteOption,
+  CalciteSelect,
+  CalciteTooltip,
+} from "@esri/calcite-components-react";
+import { useEffect, useState } from "react";
+import useActiveFormat from "./useActiveFormat";
+import useActiveDisplayCoordinate from "./useActiveDisplayCoordinate";
+import "./SimpleCoordinateConversion.css";
+
+interface SimpleCoordinateConversionProps {
+  vm: CoordinateConversionViewModel;
+}
+
+/**
+ * Custom component to display map lat/lon coordinates
+ */
+export default function SimpleCoordinateConversion(
+  props: SimpleCoordinateConversionProps
+) {
+  const [mode, setMode] = useState("live");
+
+  const { vm } = props;
+
+  useEffect(() => {
+    mode === "live" ? (vm.mode = "live") : (vm.mode = "capture");
+  }, [mode, vm]);
+
+  const { activeFormat } = useActiveFormat(vm);
+  const { activeDisplayCoordinate } = useActiveDisplayCoordinate(vm);
+
+  async function reverseConvert(value: string) {
+    try {
+      if (activeFormat) {
+        const point = await vm.reverseConvert(value, activeFormat);
+        vm.view.goTo(point);
+      }
+    } catch (e) {
+      // Swallow errors
+    }
+  }
+
+  return (
+    <div className="simple-coordinate-conversion">
+      <CalciteButton
+        id="simple-coordinate-change-mode"
+        iconStart={mode === "live" ? "pin-tear" : "pin-tear-f"}
+        appearance={mode === "live" ? "outline-fill" : "solid"}
+        kind="neutral"
+        scale="s"
+        onClick={() => {
+          mode === "live" ? setMode("capture") : setMode("live");
+        }}
+      />
+      <CalciteTooltip referenceElement="simple-coordinate-change-mode">
+        {mode === "live"
+          ? "Show live position on mouse move"
+          : "Capture position on mouse click"}
+      </CalciteTooltip>
+      <CalciteSelect
+        class="simple-coordinate-conversion-select"
+        id="simple-coordinate-select"
+        label="select"
+        onCalciteSelectChange={(e) => {
+          const f = vm.formats.find((format) => format.name === e.target.value);
+          const newConversion = new Conversion({ format: f });
+          vm.conversions.removeAt(0);
+          vm.conversions.add(newConversion, 0);
+        }}
+      >
+        {vm.formats.map((format) => {
+          return (
+            <CalciteOption
+              key={format.name}
+              value={format.name}
+              selected={format.name === activeFormat?.name ? true : undefined}
+            >
+              {format.name.toLocaleUpperCase()}
+            </CalciteOption>
+          );
+        })}
+      </CalciteSelect>
+      <CalciteTooltip referenceElement="simple-coordinate-select">
+        {activeFormat?.label}
+      </CalciteTooltip>
+      <CalciteInlineEditable
+        id="simple-coordinate-editable"
+        className="simple-coordinate-editable"
+        controls
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            reverseConvert((e.target as HTMLCalciteInputElement).value);
+          }
+        }}
+        onCalciteInlineEditableEditConfirm={(e) => {
+          reverseConvert(
+            (e.target as unknown as HTMLCalciteInputElement).value
+          );
+        }}
+      >
+        <CalciteInput
+          id="simple-coordinate-input"
+          placeholder="Click on map"
+          value={activeDisplayCoordinate}
+        >
+          {activeDisplayCoordinate}
+        </CalciteInput>
+      </CalciteInlineEditable>
+      <CalciteTooltip referenceElement="simple-coordinate-editable">
+        Enter coordinate
+      </CalciteTooltip>
+    </div>
+  );
+}
